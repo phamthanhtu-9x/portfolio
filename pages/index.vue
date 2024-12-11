747f3551bd3defd536ae4f2420596b135d8ceeba
제미소/VDC-Tu Pham <71238372+phamthanhtu-9x@users.noreply.github.com>
2024-12-11 09:26:29 +0700

    Update Head

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,11 @@
+<script lang="ts" setup>
+useHead({
+  title: 'TuPahm',
+  meta: [
+    { name: 'description', content: 'This is my portfolio' }
+  ],
+})
+</script>
 <template>
   <Container>
     Home
