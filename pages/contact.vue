56549fccd2ad086b8a675c78c70a2bba91ce362d
제미소/VDC-Tu Pham <71238372+phamthanhtu-9x@users.noreply.github.com>
2024-12-12 14:13:25 +0700

    Update responsive contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,36 +2,38 @@
 import { DotLottieVue } from '@lottiefiles/dotlottie-vue';
 
 useHead({
-  title: 'Contact me'
-})
+  title: 'Contact me',
+});
 </script>
 
 <template>
   <Container class="h-full">
     <div class="h-full flex items-center justify-center">
       <DotLottieVue
-        class="xl:w-[600px] xl:h-[600px] lg:w-[400px] lg:h-[400px]"
+        class="xl:w-[600px] xl:h-[600px] lg:w-[400px] lg:h-[400px] md:block hidden"
         autoplay
         loop
         src="/lottie/contact.lottie" />
 
-      <div class="w-96 py-6 px-4 space-y-4 bg-white shadow-md rounded-md">
+      <div class="w-96 py-6 px-4 space-y-4 bg-white sm:shadow-md rounded-md overflow-hidden">
         <div class="text-3xl font-bold">
           Contact <span class="text-primary">Me!</span>
         </div>
 
-        <FormInput type="text" label="Your name:" />
-        <FormInput type="mail" label="Your email:" />
+        <FormInput
+          type="text"
+          label="Your name:" />
+        <FormInput
+          type="mail"
+          label="Your email:" />
         <FormTextarea label="Your message:" />
 
         <div class="col-span-2 flex justify-center">
-        <SecondaryButton class="w-full">Submit</SecondaryButton>
+          <SecondaryButton class="w-full">Submit</SecondaryButton>
         </div>
       </div>
     </div>
   </Container>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
