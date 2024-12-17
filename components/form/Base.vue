01d49b17222c0112b770f3b7dee6c02afde2b41b
제미소/VDC-Tu Pham <71238372+phamthanhtu-9x@users.noreply.github.com>
2024-12-17 10:33:25 +0700

    Update css variable

--- components/form/Base.vue
+++ components/form/Base.vue
@@ -11,14 +11,14 @@ defineProps<Props>();
     <label v-if="label" class="mb-2 block">
       {{ label }}
     </label>
-    <slot class="input input px-2 py-1.5 rounded-md border-2 border-gray-200 text-md block w-full" />
+    <slot class="input input px-2 py-1.5 rounded-md border-2 border-gray-200 text-md block" />
     <ErrorMessage
       :name="name"
       class="text-red-500" />
   </div>
 </template>
 <style scoped>
-.input {
+:deep(.input) {
   width: 100%;
   outline: none;
   overflow: hidden;
@@ -26,10 +26,10 @@ defineProps<Props>();
   transition: all 0.2s;
 }
 
-.input:hover,
-.input:focus {
+:deep(.input:hover),
+:deep(.input:focus) {
   border: 2px solid var(--primary);
-  box-shadow: 0px 0px 0px 4px rgb(255, 209, 100, 20%);
+  box-shadow: 0px 0px 0px 4px var(--primary-shadow);
   background-color: white;
 }
 </style>
